51b0c176be9e3166d453044615acd285a0b5f4be
kevinsamoei <samoeikev@gmail.com>
2018-01-10 16:13:34 +0300

    Add new configuration settings

--- hc/local_settings.py
+++ hc/local_settings.py
@@ -1,10 +1,12 @@
+import os
+
 SITE_ROOT = "https://hc-logros.herokuapp.com"
 SITE_NAME = "Health Checks"
 DEFAULT_FROM_EMAIL = "healthchecks@example.org"
 
 # Email
-EMAIL_HOST = "smtp.sparkpostmail.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "SMTP_Injection"
-EMAIL_HOST_PASSWORD = "445f4db624c91fcfe5e04d29358593391648ac70"
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
