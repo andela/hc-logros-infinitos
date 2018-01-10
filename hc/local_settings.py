24c796231c886dd82e0923dff2c9069f72f30493
kevinsamoei <samoeikev@gmail.com>
2018-01-10 15:50:45 +0300

    Add email configuration settings

---
+++ hc/local_settings.py
@@ -0,0 +1,10 @@
+SITE_ROOT = "https://hc-logros.herokuapp.com"
+SITE_NAME = "Health Checks"
+DEFAULT_FROM_EMAIL = "healthchecks@example.org"
+
+# Email
+EMAIL_HOST = "smtp.sparkpostmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "SMTP_Injection"
+EMAIL_HOST_PASSWORD = "445f4db624c91fcfe5e04d29358593391648ac70"
+EMAIL_USE_TLS = True
