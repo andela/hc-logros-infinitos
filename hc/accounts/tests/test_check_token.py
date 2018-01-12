d59007bac1b82f26d0ef6c3f056711f7fcbf05a6
joyytoo <joytoo97@gmail.com>
2018-01-12 15:06:00 +0300

    Add tests for check token

--- hc/accounts/tests/test_check_token.py
+++ hc/accounts/tests/test_check_token.py
@@ -1,8 +1,10 @@
+"""Contains tests to check token"""
 from django.contrib.auth.hashers import make_password
 from hc.test import BaseTestCase
 
 
 class CheckTokenTestCase(BaseTestCase):
+    """Test check token test"""
 
     def setUp(self):
         super(CheckTokenTestCase, self).setUp()
@@ -10,19 +12,34 @@ class CheckTokenTestCase(BaseTestCase):
         self.profile.save()
 
     def test_it_shows_form(self):
-        r = self.client.get("/accounts/check_token/alice/secret-token/")
-        self.assertContains(r, "You are about to log in")
+        """Test it shows form"""
+        result = self.client.get("/accounts/check_token/alice/secret-token/")
+        self.assertContains(result, "You are about to log in")
 
     def test_it_redirects(self):
-        r = self.client.post("/accounts/check_token/alice/secret-token/")
-        self.assertRedirects(r, "/checks/")
+        """Test it redirects"""
+        result = self.client.post("/accounts/check_token/alice/secret-token/")
+        self.assertRedirects(result, "/checks/")
 
         # After login, token should be blank
         self.profile.refresh_from_db()
         self.assertEqual(self.profile.token, "")
 
     ### Login and test it redirects already logged in
+    def test_redirects_already_loggedin(self):
+        """Tests redirects already logged in login"""
+        # Login
+        self.client.login(username="alice@example.org", password="password")
+
+        # Login again, when already authenticated
+        result = self.client.post("/accounts/check_token/alice/secret-token/")
+        self.assertRedirects(result, "/checks/")
 
     ### Login with a bad token and check that it redirects
+    def test_it_redirects_bad_token(self):
+        """Tests login with  bad token"""
+        url = "/accounts/check_token/alice/bad-token/"
+        result = self.client.post(url, follow=True)
+        self.assertRedirects(result, "/accounts/login/")
 
     ### Any other tests?
