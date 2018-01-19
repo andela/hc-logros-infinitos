d50a18042ce4b32b0263a6565c6932914367f700
kevinsamoei <samoeikev@gmail.com>
2018-01-19 14:22:36 +0300

    Cpnfigure db to use postgres

--- hc/settings.py
+++ hc/settings.py
@@ -84,9 +84,11 @@ TEST_RUNNER = 'hc.api.tests.CustomRunner'
 # install requirements.txt and do manage.py runserver and it works
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':   './hc.sqlite',
-    }
+            'ENGINE':   'django.db.backends.postgresql',
+            'NAME':     'hc',
+            'USER':     'postgres',
+            'TEST': {'CHARSET': 'UTF8'}
+            }
 }
 
 # You can switch database engine to postgres or mysql using environment
