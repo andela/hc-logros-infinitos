06c6d20360ca859e8ebb354e3d9a2d327cbb0c57
joyytoo <joytoo97@gmail.com>
2018-01-23 17:07:42 +0300

    Change db config

--- hc/settings.py
+++ hc/settings.py
@@ -128,7 +128,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-SITE_ROOT = "http://localhost:8000"
+SITE_ROOT = "https://hc-logros.herokuapp.com"
 SITE_NAME = "Health Checks"
 
 PING_ENDPOINT = SITE_ROOT + "/ping/"
