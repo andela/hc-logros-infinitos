be5568d7413abfa160cabd596740b03a9b0ef45b
Shakira Seruwagi <shakiraseruwagi@Shakiras-MacBook-Pro.local>
2018-01-31 10:02:50 +0300

    Add front models

--- hc/front/models.py
+++ hc/front/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User)
+    body = models.TextField(max_length=500)
+    category = models.ForeignKey(Category, null=True)
+    created_date = models.DateTimeField(null=True)
+
